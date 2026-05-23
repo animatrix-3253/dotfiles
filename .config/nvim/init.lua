9a109c8640d0f7f16b50f008a3c1fc390207a3f8
animatrix-3253 <wpcf160+github@gmail.com>
2026-05-23 06:55:35 -0500

    initial commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+require("config.lazy") 
+
+-- lines
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- set the tab width
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- colorscheme
+vim.opt.termguicolors = false -- change this to true when in actually terminal emulator
+
+vim.o.background = "dark"
+vim.cmd[[colorscheme gruvbox]]
